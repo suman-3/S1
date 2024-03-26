e825ac12af26f05427e2243b35395a35bfabe6a5
Palash <ng.palash.das@gmail.com>
2024-03-26 21:24:10 +0530

    quality enhanced

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,7 +1,7 @@
-import Hero from '@/components/hero'
-import FAQ from '@/components/faq'
-import Testimonials from '@/components/testimonials'
-import Newsletter from '@/components/newsletter'
+import Hero from '@/components/Home/hero'
+import FAQ from '@/components/Home/faq'
+import Testimonials from '@/components/Home/testimonials'
+import Newsletter from '@/components/Home/newsletter'
 
 export default function Home() {
   return (
---
+++ components/Tracks/tracks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const tracks = () => {
+  return (
+    <div><h1>tracks</h1></div>
+  )
+}
+
+export default tracks
