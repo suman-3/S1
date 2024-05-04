25330a669c638ac48f49973bde9c9373b465dbbb
Avik Agarwala <avikagarwala@outlook.com>
2024-05-04 15:37:58 +0530

    Update theme section

--- components/Home/Themes.tsx
+++ components/Home/Themes.tsx
@@ -1,29 +1,29 @@
 "use client";
 
 import CategoryCard from "@/components/Card/TracksCard";
-import CATEGORIES from "@/data/tracks";
+import CATEGORIES from "@/data/themes";
 import MaxWidthWrapper from "../Wrapper/MaxWidthWrapper";
 
-export function CoursesCategories() {
+export function Themes() {
   return (
     <MaxWidthWrapper>
-      <section className="container mx-auto px-8 pt-20" id="Tracks">
+      <section className="container mx-auto px-8 pt-20" id="Themes">
         <div className="mb-10 grid place-items-center text-center">
-          <h1 className="h3 font-bold mt-5 mb-10">Tracks</h1>
+          <h1 className="h3 font-bold mt-5 mb-10">Themes</h1>
         </div>
         <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
           <div className="col-span-1 flex flex-col gap-6">
-            {CATEGORIES.slice(0, 4).map((props, key) => (
+            {CATEGORIES.slice(0, 2).map((props, key) => (
               <CategoryCard key={key} {...props} />
             ))}
           </div>
           <div className="col-span-1 flex flex-col gap-6">
-            {CATEGORIES.slice(4, 9).map((props, key) => (
+            {CATEGORIES.slice(3, 5).map((props, key) => (
               <CategoryCard key={key} {...props} />
             ))}
           </div>
           <div className="col-span-1 flex flex-col gap-6">
-            {CATEGORIES.slice(9, 13).map((props, key) => (
+            {CATEGORIES.slice(6, 8).map((props, key) => (
               <CategoryCard key={key} {...props} />
             ))}
           </div>
@@ -33,4 +33,4 @@ export function CoursesCategories() {
   );
 }
 
-export default CoursesCategories;
+export default Themes;
