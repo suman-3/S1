b81f4c08f4b6c713aa4f54a94abb6ef31cef763f
Palash <ng.palash.das@gmail.com>
2024-05-06 18:01:38 +0530

    prize changes

--- app/prizepool/page.tsx
+++ app/prizepool/page.tsx
@@ -11,6 +11,7 @@ const PricePool = () => {
         <div className="max-w-3xl mx-auto text-center pb-12 md:pb-10">
           <h2 className="h2 mb-1">Prize Pool</h2>
         </div>
+        <h4 className="h4 mb-1">Ultimate Prize</h4>
         <div className="flex flex-row justify-between items-center">
           <div className="body">
             <div className="nft flex justify-center items-center">
@@ -20,19 +21,17 @@ const PricePool = () => {
                   src="/images/prizepool/silver.png"
                   alt="NFT"
                 />
-                <h2 className="text-3xl text-[#7B8185] font-semibold">2nd Prize</h2>
+                <h2 className="text-3xl text-[#7B8185] font-semibold">
+                  2nd Prize
+                </h2>
                 <p className="description">
                   Our Kibertopiks will give you nothing, waste your money on us.
                 </p>
                 <div className="tokenInfo">
                   <div className="price">
-                    <ins>◘</ins>
-                    <p>0.031 ETH</p>
+                    <p>INR 15,000</p>
                   </div>
-                  <div className="duration">
-                    <ins>◷</ins>
-                    <p>11 days left</p>
-                  </div>
+                  <div className="duration"></div>
                 </div>
               </div>
             </div>
@@ -45,19 +44,17 @@ const PricePool = () => {
                   src="/images/prizepool/gold.png"
                   alt="NFT"
                 />
-                <h2 className="text-3xl text-[#EFA604] font-semibold">1st Prize</h2>
+                <h2 className="text-3xl text-[#EFA604] font-semibold">
+                  1st Prize
+                </h2>
                 <p className="description">
                   Our Kibertopiks will give you nothing, waste your money on us.
                 </p>
                 <div className="tokenInfo">
                   <div className="price">
-                    <ins>◘</ins>
-                    <p>0.031 ETH</p>
+                    <p>INR 25,000</p>
                   </div>
-                  <div className="duration">
-                    <ins>◷</ins>
-                    <p>11 days left</p>
-                  </div>
+                  <div className="duration"></div>
                 </div>
               </div>
             </div>
@@ -70,19 +67,17 @@ const PricePool = () => {
                   src="/images/prizepool/bronze.png"
                   alt="NFT"
                 />
-                <h2 className="text-3xl font-semibold text-[#D9A05D]">3rd Prize</h2>
+                <h2 className="text-3xl font-semibold text-[#D9A05D]">
+                  3rd Prize
+                </h2>
                 <p className="description">
                   Our Kibertopiks will give you nothing, waste your money on us.
                 </p>
                 <div className="tokenInfo">
                   <div className="price">
-                    <ins>◘</ins>
-                    <p>0.031 ETH</p>
+                    <p>INR 10,000</p>
                   </div>
-                  <div className="duration">
-                    <ins>◷</ins>
-                    <p>11 days left</p>
-                  </div>
+                  <div className="duration"></div>
                 </div>
               </div>
             </div>
