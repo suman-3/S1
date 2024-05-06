47ae91cb67339df5ed360bf5cdd5030035ab0e41
Atmajo <atmajoc@gmail.com>
2024-05-06 15:05:04 +0530

    basic changes

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -37,8 +37,8 @@ export default function Hero() {
               id="illustration-01"
             >
               <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
+              <stop stopColor="#b8b8b8" offset="77.402%" />
+              <stop stopColor="#808080" offset="100%" />
             </linearGradient>
           </defs>
           <g fill="url(#illustration-01)" fillRule="evenodd">
@@ -94,7 +94,7 @@ export default function Hero() {
               </div>
             </div>
           </div>
-
+          
           {/* Hero image */}
           <ModalVideo
             thumb={VideoThumb}
