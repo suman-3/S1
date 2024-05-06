0278f150c0bdf1d8ce8136e3905c788c0bdf16ee
Palash <ng.palash.das@gmail.com>
2024-05-06 17:17:13 +0530

    faq changed

--- components/Home/Faq/Faq.tsx
+++ components/Home/Faq/Faq.tsx
@@ -4,7 +4,7 @@ export default function FAQ() {
   return (
     <section className="relative">
       <div
-        className="absolute inset-0 top-1/2 md:mt-24 lg:mt-0 bg-blue-400 pointer-events-none"
+        className="absolute inset-0 top-1/2 md:mt-24 lg:mt-0 pointer-events-none"
         aria-hidden="true"
       ></div>
       {/* <div className="absolute left-0 right-0 bottom-0 m-auto w-px p-px h-20 bg-gray-200 transform translate-y-1/2"></div> */}
--- data/faq.ts
+++ data/faq.ts
@@ -6,39 +6,45 @@ const faq = [
   },
   {
     key: 2,
+    question: "What is the maximum/minimum team size?",
+    answer:
+      "The team size can range from 1 to 4 participants. Please note that cross institutional team is not allowed.",
+  },
+  {
+    key: 3,
     question: "What is the participation fee?",
     answer: "Innofusion Hackathon is absolutely free to register.",
   },
   {
-    key: 3,
-    question: "What is the maximum/minimum team size?",
-    answer: "The team size can range from 1 to 4 participants. Please note that cross institutional team is not allowed.",
-  },
-  {
     key: 4,
     question: "Can I participate solo?",
-    answer: "We believe in the power of teamwork, but if you prefer to participate solo, you're welcome to do so.",
+    answer:
+      "We believe in the power of teamwork, but if you prefer to participate solo, you're welcome to do so.",
   },
   {
     key: 5,
-    question: "Can I participate remotely?",
-    answer: "The idea submission round is conducted online, allowing remote participation. However, please note that the finale round is held offline.",
+    question: "Where do I have to go for the finale?",
+    answer:
+      "The finale will be held at University Of Engineering and Management, University Area, Action Area III, New Town, Kolkata - 700160.",
   },
   {
     key: 6,
-    question: "Where do I have to go for the finale?",
-    answer: "The finale will be held at University Of Engineering and Management, University Area, Action Area III, New Town, Kolkata - 700160.",
+    question: "Can I participate remotely?",
+    answer:
+      "The idea submission round is conducted online, allowing remote participation. However, please note that the finale round is held offline.",
   },
   {
     key: 7,
     question: "Do we have to bring our own food?",
-    answer: "If you prefer to, you can bring your own food, but we'll also be providing food and beverages.",
+    answer:
+      "If you prefer to, you can bring your own food, but we'll also be providing food and beverages.",
   },
-  
+
   {
     key: 8,
     question: "Can we leave the campus before completion of the hackathon?",
-    answer: "Stay on campus until you submit your work. Leaving before submitting will cause disqualification. Once done, you can leave but come back for results. Outdoor breaks are allowed only during designated times, but at your own risk.",
+    answer:
+      "Stay on campus until you submit your work. Leaving before submitting will cause disqualification. Once done, you can leave but come back for results. Outdoor breaks are allowed only during designated times, but at your own risk.",
   },
   {
     key: 9,
