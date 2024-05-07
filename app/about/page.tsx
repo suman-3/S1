a16da67a7b96c8c77ce0d0a89c9b29caa274b774
Palash <ng.palash.das@gmail.com>
2024-05-07 14:41:54 +0530

    about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,263 +1,62 @@
-"use client";
-
-import { useState, useRef, useEffect } from "react";
-import { Transition } from "@headlessui/react";
+import MaxWidthWrapper from "@/components/Wrapper/MaxWidthWrapper";
 import Image from "next/image";
-import FeaturesBg from "@/public/images/features-bg.png";
-import FeaturesElement from "@/public/images/features-element.png";
 
 export default function About() {
-  const [tab, setTab] = useState<number>(1);
-  const tabs = useRef<HTMLDivElement>(null);
-  const heightFix = () => {
-    if (tabs.current && tabs.current.parentElement)
-      tabs.current.parentElement.style.height = `${tabs.current.clientHeight}px`;
-  };
-  
-  useEffect(() => {
-    heightFix();
-  }, []);
-  
   return (
-    <section className="relative my-10">
-      <div
-        className="absolute inset-0 bg-gray-100 pointer-events-none mb-16"
-        aria-hidden="true"
-      ></div>
-      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
+    <MaxWidthWrapper>
+      <section className="relative my-5">
+        <div
+          className="absolute inset-0 pointer-events-none mb-16"
+          aria-hidden="true"
+        ></div>
+        <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
 
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-12 md:pt-20">
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h6 className="mb-4 uppercase !text-gray-500 font-semibold">
-              About us
-            </h6>
-            <h2 className="h4 mb-1">InnoFusion Hackathon</h2>
-            <p className="text-xl text-gray-600">Organized By University of Engineering & Management, Kolkata</p>
-          </div>
-
-          <div className="md:grid md:grid-cols-12 md:gap-6">
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6"
-              data-aos="fade-right"
-            >
-              <div className="md:pr-4 lg:pr-12 xl:pr-16 mb-8">
-                <h3 className="h3 mb-3">Where hardware meets software.</h3>
-                <p className="text-xl text-gray-600">
-                  These Problem statements are based on the hardware and the
-                  software implication in real life scenario :-
+        <div className="relative mx-auto px-4 sm:px-6">
+          <div className="pt-12 md:pt-20">
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+              <h6 className="mb-4 uppercase !text-gray-500 font-semibold">
+                About us
+              </h6>
+              <h2 className="h3 mb-2">InnoFusion Hackathon</h2>
+              <p className="text-xl text-gray-600">
+                Organized By University of Engineering & Management, Kolkata
+              </p>
+            </div>
+            <h2 className="h4 mb-4 text-center">
+              University of Engineering & Management
+            </h2>
+            <div className="flex max-lg:flex-wrap">
+              <div>
+                <p className="text-gray-600 max-w-2xl text-justify">
+                  The University of Engineering and Management (UEM) in Kolkata
+                  is renowned for its commitment to excellence in education and
+                  research in engineering and technology. With state-of-the-art
+                  facilities and a dynamic academic environment, UEM offers a
+                  diverse range of undergraduate and postgraduate programs. Its
+                  strategic location in Kolkata, a hub of culture and industry,
+                  provides students with unique opportunities for experiential
+                  learning and professional growth. Committed to nurturing
+                  innovation and entrepreneurship, UEM equips students with the
+                  skills and knowledge to thrive in a rapidly evolving global
+                  landscape. Through a blend of academic rigour and practical
+                  exposure, UEM prepares students to become future leaders and
+                  innovators in their respective fields.
                 </p>
               </div>
-              <div className="mb-8 md:mb-0">
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 1
-                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
-                      : "bg-gray-200 border-transparent"
-                  }`}
-                  href="#0"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(1);
-                  }}
-                >
-                  <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">
-                      Hardware Problem Statements.
-                    </div>
-                    <div className="text-gray-600">
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg
-                      className="w-3 h-3 fill-current"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z" />
-                    </svg>
-                  </div>
-                </a>
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 2
-                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
-                      : "bg-gray-200 border-transparent"
-                  }`}
-                  href="#0"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(2);
-                  }}
-                >
-                  <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">
-                      Hardware + software Problem Statements.
-                    </div>
-                    <div className="text-gray-600">
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg
-                      className="w-3 h-3 fill-current"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z"
-                        fillRule="nonzero"
-                      />
-                    </svg>
-                  </div>
-                </a>
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 3
-                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
-                      : "bg-gray-200 border-transparent"
-                  }`}
-                  href="#0"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(3);
-                  }}
-                >
-                  <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">
-                      Software Problem Statements.
-                    </div>
-                    <div className="text-gray-600">
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg
-                      className="w-3 h-3 fill-current"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z"
-                        fill="#191919"
-                        fillRule="nonzero"
-                      />
-                    </svg>
-                  </div>
-                </a>
-              </div>
+              <Image
+                src="/images/uem.jpg"
+                alt="Uem Image"
+                width={400}
+                height={400}
+                className="rounded-xl h-[200px] w-[400px] object-cover lg:ml-4 opacity-80 max-lg:mt-5"
+              />
             </div>
-
-            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1">
-              <div className="transition-all">
-                <div
-                  className="relative flex flex-col text-center lg:text-right"
-                  data-aos="zoom-y-out"
-                  ref={tabs}
-                >
-                  <Transition
-                    show={tab === 1}
-                    appear={true}
-                    className="w-full"
-                    enter="transition ease-in-out duration-700 transform order-first"
-                    enterFrom="opacity-0 translate-y-16"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in-out duration-300 transform absolute"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 -translate-y-16"
-                    beforeEnter={() => heightFix()}
-                    unmount={false}
-                  >
-                    <div className="relative inline-flex flex-col">
-                      <Image
-                        className="md:max-w-none mx-auto rounded"
-                        src={FeaturesBg}
-                        width={500}
-                        height="462"
-                        alt="Features bg"
-                      />
-                      <Image
-                        className="md:max-w-none absolute w-full left-0 transform animate-float"
-                        src={FeaturesElement}
-                        width={500}
-                        height="44"
-                        alt="Element"
-                        style={{ top: "30%" }}
-                      />
-                    </div>
-                  </Transition>
-                  <Transition
-                    show={tab === 2}
-                    appear={true}
-                    className="w-full"
-                    enter="transition ease-in-out duration-700 transform order-first"
-                    enterFrom="opacity-0 translate-y-16"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in-out duration-300 transform absolute"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 -translate-y-16"
-                    beforeEnter={() => heightFix()}
-                    unmount={false}
-                  >
-                    <div className="relative inline-flex flex-col">
-                      <Image
-                        className="md:max-w-none mx-auto rounded"
-                        src={FeaturesBg}
-                        width={500}
-                        height="462"
-                        alt="Features bg"
-                      />
-                      <Image
-                        className="md:max-w-none absolute w-full left-0 transform animate-float"
-                        src={FeaturesElement}
-                        width={500}
-                        height="44"
-                        alt="Element"
-                        style={{ top: "30%" }}
-                      />
-                    </div>
-                  </Transition>
-                  <Transition
-                    show={tab === 3}
-                    appear={true}
-                    className="w-full"
-                    enter="transition ease-in-out duration-700 transform order-first"
-                    enterFrom="opacity-0 translate-y-16"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in-out duration-300 transform absolute"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 -translate-y-16"
-                    beforeEnter={() => heightFix()}
-                    unmount={false}
-                  >
-                    <div className="relative inline-flex flex-col">
-                      <Image
-                        className="md:max-w-none mx-auto rounded"
-                        src={FeaturesBg}
-                        width={500}
-                        height="462"
-                        alt="Features bg"
-                      />
-                      <Image
-                        className="md:max-w-none absolute w-full left-0 transform animate-float"
-                        src={FeaturesElement}
-                        width={500}
-                        height="44"
-                        alt="Element"
-                        style={{ top: "30%" }}
-                      />
-                    </div>
-                  </Transition>
-                </div>
-              </div>
-            </div>
+            <h2 className="h4 mt-10 mb-1 text-center">
+              InnoFusion Hackathon Season 1
+            </h2>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </MaxWidthWrapper>
   );
 }
